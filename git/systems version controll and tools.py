ee6d004892db7842ee8dad91a6643ad320eb15e3
Elvis <66243414+Elvis-1@users.noreply.github.com>
2023-12-05 10:01:16 +0100

    updated

---
+++ git/systems version controll and tools.py
@@ -0,0 +1,15 @@
+'''
+There are many different version control systems available. For example, Subversion, Perforce, AWS Code Commit, Mercurial, and Git to name a few.
+
+Version control systems can be split into two types or categories. Centralized version control systems and distributed version control systems
+
+ Centralized version control systems, or CVCS for short, contain a server and a client. The server contains the main repository that houses the full history of versions of the code base. Developers working on projects using a centralized system need to pull down the code from the server to their local machine. This gives the user their own working copy of the code base. The server holds the full history of changes.
+
+ Distributed version control systems or DVCS for short, are similar to the centralized model. You still need to pull code down from the server to view the latest changes. The key difference is that every user is essentially a server and not a client. This means that every time you pull down code from the distributed model, you have the entire history of changes on your local system.
+
+  let's explore some of the advantages and disadvantages of each. The advantage of CVCS is that they are considered easier to learn than their distributed counterparts. They also give more access controls to users. The disadvantage is that they can be slower given that you need to establish a connection to the server to perform any actions. With DVCS, you don't need to be connected to the server to add your changes or view a file's history. It works as if you are actually connected to the server directly but on your own local machine. You only ever need to connect to the server to pull down the latest changes or to push your own changes. It essentially allows users to work in an offline state
+
+  Speed and performance are also better than its CVCS counterpart. DVCS is a key factor in improving developer operations and the software development life cycle
+
+ 
+'''
---
+++ git/vc in prof softdev.py
@@ -0,0 +1,19 @@
+'''
+  Workflow
+
+  Workflows are essential to ensure code is managed correctly and reduce mistakes from happening. Different projects will have different workflows
+
+  Continuous Integration
+
+  Continuous Integration, or CI, is used to automate the integration of code changes from multiple developers into a single main stream. Using a workflow whereby small changes are merged frequently, often many times per day, will reduce the number of merge conflicts.
+
+Continuous Delivery
+
+Continuous Delivery is an extension of Continuous Integration. Once the changes have been merged into the main stream, a Continuous Delivery system automatically packages the application and prepares it for deployment. This helps avoid human error when packaging the application.
+
+Continuous Deployment
+
+Continuous Deployment is an extension of Continuous Delivery. The goal of Continuous Deployment is to deploy and release software to customers frequently and safely. The strategy commonly involves automatically deploying to a test (also known as staging) environment first to validate the deployment package and software changes. Once validated, it can automatically deploy to the live (also known as production) environment for customers.
+
+
+'''
