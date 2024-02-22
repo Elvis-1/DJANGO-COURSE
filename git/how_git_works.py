71a0252483383198eda127eb6e4a005910a91f7e
Elvis <66243414+Elvis-1@users.noreply.github.com>
2024-02-22 10:00:00 +0100

    updated

---
+++ git/how_git_works.py
@@ -0,0 +1,13 @@
+'''
+As you now know, GitHub is a cloud based hosting service, that lets you manage Git repositories from a user interface. It's like a social network. You can follow users or accept code contributions from anywhere in the world
+
+Let's start with the README.md file. This item was added when I created the repository on GitHub. The other item, is a folder called .git, which is a hidden folder used to track all the changes. In Linux, any folder starting with a dot is a hidden folder. 
+
+The .git folder is initialized by running the git INT command. As the repository was created on GitHub, it was not required for us to run it
+
+Git uses workflows which can be broken into three states namely, modified, staged, and committed.
+
+Let's start with the first state, adding removing and updating any file inside the repository is considered a modified state. Git knows the file has changed, but does not track it. This is where the staging state comes in. Let's turn to it now. In order for Git to track a file, it needs to be put in the staged area. Once added, any modifications are tracked. Which offers a security blanket prior to committing the changes.
+
+Then, the last state is the committed state. Committing a file in Git is like a save point in many ways. Git will save the file, and have a snapshot of the current changes
+'''
